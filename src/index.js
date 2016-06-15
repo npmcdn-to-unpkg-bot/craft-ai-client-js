f9d1c1a74e0497ad5019c0c83f47888ef38462bc
Clodéric Mars <cloderic.mars@gmail.com>
2016-06-15 09:40:35 +0200

    Consistant import for `craft-ai-interpreter`

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import decide, { Time } from 'craft-ai-interpreter';
+import { decide, Time } from 'craft-ai-interpreter';
 import * as errors from './errors';
 import createClient from './client';
 import DEFAULT from './defaults';
